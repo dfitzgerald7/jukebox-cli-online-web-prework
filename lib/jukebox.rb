f1e0fb2110503515af2007e55414da21f7abb039
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-09-19 17:06:03 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -42,6 +42,7 @@ def run(songs)
   help 
   stop_loop = false 
   until stop_loop == true
+    puts "Please enter command:"
     user_input = gets.chomp 
     if user_input.downcase == "help"
       help 
