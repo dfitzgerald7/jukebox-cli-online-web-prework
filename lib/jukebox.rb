09e1f21f370053d6b9917776cc1d4900c0822c56
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-09-19 17:16:38 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -26,7 +26,7 @@ def play(songs)
   if songs.include?(song)
     puts "Playing #{song}"
   elsif (1..9).include?(song.to_i)
-    puts "Playing #{songs[song_to.i - 1]}"
+    puts "Playing #{songs[song.to_i - 1]}"
   else 
     puts "Invalid input, please try again"
   end 
